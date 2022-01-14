94d8d9dfd560aa8ee458f5a1d227df0ce420ba3b
Andy Feetenby <andyfeetenby@gmail.com>
2022-01-14 09:13:28 +0700

    Dynamically set active tab on Profile Page

--- controllers/userController.js
+++ controllers/userController.js
@@ -113,6 +113,7 @@ exports.profilePostsScreen = function(req, res){
     // ask our Post model for post by author ID
     Post.findByAuthorId(req.profileUser._id).then(function(posts){
         res.render('profile', {
+            currentPage: "posts",
             posts: posts,
             profileUsername: req.profileUser.username,
             profileAvatar: req.profileUser.avatar,
@@ -129,6 +130,7 @@ exports.profileFollowersScreen = async function(req, res) {
     try {
         let followers = await Follow.getFollowersById(req.profileUser._id)
         res.render('profile-followers', {
+            currentPage: "followers",
             followers: followers,
             profileUsername: req.profileUser.username,
             profileAvatar: req.profileUser.avatar,
@@ -145,6 +147,7 @@ exports.profileFollowingScreen = async function(req, res) {
     try {
         let following = await Follow.getFollowingById(req.profileUser._id)
         res.render('profile-following', {
+            currentPage: "following",
             following: following,
             profileUsername: req.profileUser.username,
             profileAvatar: req.profileUser.avatar,
