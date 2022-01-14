86e686a7217e8ed633c6e434f73f0a0467479c76
Andy Feetenby <andyfeetenby@gmail.com>
2022-01-14 14:49:35 +0700

    Added Dynamic ejs displaying Counts for Posts, Followers, Following

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,6 +66,20 @@ exports.sharedProfileData = async function (req, res, next){
     }
     req.isVisitorsProfile = isVisitorsProfile
     req.isFollowing = isFollowing
+
+    // retrieve post, follower, and following counts
+    let postCountPromise =  Post.countPostsByAuthor(req.profileUser._id)
+    let followerCountPromise =  Follow.countFollowersById(req.profileUser._id)
+    let followingCountPromise =  Follow.countFollowingById(req.profileUser._id)
+    
+    
+    // destructure array
+    let [postCount, followerCount, followingCount] = await Promise.all([postCountPromise, followerCountPromise, followingCountPromise])
+    
+    req.postCount = postCount
+    req.followerCount = followerCount
+    req.followingCount = followingCount
+
     next()
 }
 
@@ -118,7 +132,8 @@ exports.profilePostsScreen = function(req, res){
             profileUsername: req.profileUser.username,
             profileAvatar: req.profileUser.avatar,
             isFollowing: req.isFollowing,
-            isVisitorsProfile: req.isVisitorsProfile
+            isVisitorsProfile: req.isVisitorsProfile,
+            counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount }
         })
     }).catch(function(){
         res.render("404")
@@ -135,7 +150,9 @@ exports.profileFollowersScreen = async function(req, res) {
             profileUsername: req.profileUser.username,
             profileAvatar: req.profileUser.avatar,
             isFollowing: req.isFollowing,
-            isVisitorsProfile: req.isVisitorsProfile
+            isVisitorsProfile: req.isVisitorsProfile,
+            counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount }
+
         })
     } catch {
         res.render("404")
@@ -152,7 +169,8 @@ exports.profileFollowingScreen = async function(req, res) {
             profileUsername: req.profileUser.username,
             profileAvatar: req.profileUser.avatar,
             isFollowing: req.isFollowing,
-            isVisitorsProfile: req.isVisitorsProfile
+            isVisitorsProfile: req.isVisitorsProfile,
+            counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount }
         })
     } catch {
         res.render("404")
