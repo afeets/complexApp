06024250c84eedfff8e9447a7cb3ef97cefe4e28
Andy Feetenby <andyfeetenby@gmail.com>
2022-01-24 17:00:51 +0700

    Added APIs to delete post, and get posts by author. Also enabled cors allowing across all domains

--- router-api.js
+++ router-api.js
@@ -3,6 +3,10 @@ const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
 const followController = require('./controllers/followController')
 
+// allow api to be used from any domain
+const cors = require('cors')
+apiRouter.use(cors())
+
 apiRouter.post('/login', userController.apiLogin)
 apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreate)
 apiRouter.delete('/post/:id', userController.apiMustBeLoggedIn, postController.apiDelete)
